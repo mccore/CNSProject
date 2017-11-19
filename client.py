ed6e5e047876dd17eb382b9b5de0476ad6314c76
mccore <edwardmccorry@aol.com>
2017-11-19 18:09:51 -0500

    added more ssl stuff and fixed the client code

--- client.py
+++ client.py
@@ -5,14 +5,17 @@ import os
 import hashlib
 import sha3
 import ssl
+import time
 
 #This whole thing needs to use encryption and probably certs
 
 class Server(Thread):
-  def __init__(self, port, hash_list):
+  def __init__(self, port, hash_list, ssl_certfile, ssl_keyfile):
     Thread.__init__(self)
     self.hash_list = hash_list
     self.bufsize = 65535
+    self.ssl_certfile = ssl_certfile
+    self.ssl_keyfile = ssl_keyfile
 
     self.socket = socket(AF_INET, SOCK_STREAM)
     self.socket.setsockopt(SOL_SOCKET, SO_REUSEPORT, 1)
@@ -30,45 +33,56 @@ class Server(Thread):
 
 
 class Client(Thread):
-  def __init__(self,host,port, hash_list):
+  def __init__(self,host,port, hash_list, ssl_certfile):
     Thread.__init__(self)
     self.port = port
     self.host = host
     self.bufsize = 1024
     self.addr = (host,port)
     self.hash_list = hash_list
+    self.ssl_certfile = ssl_certfile
     self.socket = socket(AF_INET , SOCK_STREAM)
 
   def run(self):
-    invalid = True
     while invalid:
       try:
-        invalid = False
-        self.ssl_sock = ssl.wrap_socket(self.socket, ca_certs=ssl_certfile, cert_reqs=ssl.CERT_REQUIRED )
-        self.ssl_sock.connect(self.addr)
-      except:
-        invalid = True
+        self.ssl_sock = ssl.wrap_socket(self.socket, ca_certs=self.ssl_certfile, cert_reqs=ssl.CERT_REQUIRED)
+        print("Wrapped client socket for SSL")
+      except socket.error:
+        print("Error wrapping SSL socket")
+        exit(1)
 
-    #This needs to change. We should be receiving the hash list from the other side which may need to be in a recv loop.
+      try:
+        self.ssl_sock.connect(self.addr)
+        print("Client socket connected")
+      except socket.error, msg:
+        print("Error on client socket connect")
+        exit(1)
+
+    #We should be receiving the hash list from the other side which may need to be in a recv loop.
+    new_hash_list = []
     while True:
-      data = raw_input('> ')
-      if not data:
-        continue
-      data = name+' said : '+data
-      tcpClient.send(data)
+      try:
+        new_hash_list.append(self.ssl_sock.recv(1024))
+      except:
+        print("Error on recv()")
+        exit(1)
+
+    self.socket.close()
+    self.ssl_sock.close()
 
     #We now need to compare the hash_list we received to the hash_list that we already have. Then print out the common hashes and their total number.
 
 def main():
-  print(sys.argv)
   if len(sys.argv) != 5:
     print ('usage: python3 client.py <destination IP> <destination port> <source port> <directory of files>')
+
   destIP = sys.argv[1]
   destPORT = sys.argv[2]
   sourcePORT = sys.argv[3]
   directory = sys.argv[4]
 
-  os.system("openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365")
+  os.system("openssl req -x509 -nodes -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -subj '/CN=localhost'")
   ssl_certfile = "./cert.pem"
   ssl_keyfile = "./key.pem"
 
@@ -85,14 +99,17 @@ def main():
       hash_list.append(hasher)
 
 
-  server = Server(sourcePORT, hash_list)
-  client = Client(destIP, destPORT, hash_list)
+  server = Server(sourcePORT, hash_list, ssl_certfile, ssl_keyfile)
+  client = Client(destIP, destPORT, hash_list, ssl_certfile)
 
   server.start()
   client.start()
 
+  while client.isAlive() and server.isAlive():
+    time.sleep(0.100)
   #No idea what this is for
-  server.join()
+  #server.join()
+
 
 if __name__ == "__main__":
   main()
